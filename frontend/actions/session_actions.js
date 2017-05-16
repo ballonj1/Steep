22da647b311c1dce3117b6d3bc1fd5dcc156c9d9
John Ballon <ballonj1@tcnj.edu>
2017-05-16 16:40:18 -0700

    Write session actions and reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,5 +1,31 @@
-import { login, logout, signup } from '../util/session_api_util'
-export const login = (user) => (dispatch) => {
+import * as APIUtil from '../util/session_api_util'
 
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS'
 
-}
+export const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+})
+
+export const receiveErrors = (errors) =>  ({
+  type: RECEIVE_ERRORS,
+  errors
+})
+
+export const login = (user) => (dispatch) => (
+  APIUtil.login(user)
+    .then((loggedInUser) => dispatch(receiveCurrentUser(loggedInUser)),
+          (errors) => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const signup = (user) => (dispatch) => (
+  APIUtil.signup(user)
+    .then((signedUser) => dispatch(receiveCurrentUser(signedUser)),
+          (errors) => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const logout = () => (dispatch) => (
+  APIUtil.logout()
+    .then((loggedOutUser) => dispatch(receiveCurrentUser(null)))
+)
--- frontend/tea_with_danger.jsx
+++ frontend/tea_with_danger.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { signup, login, logout } from './util/session_api_util'
+import configureStore from './store/store';
+import { signup, login, logout } from './actions/session_actions'
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
   ReactDom.render(<h1>Hello there</h1>, root);
 });
