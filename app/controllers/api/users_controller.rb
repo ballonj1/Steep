c91a4e4b5977b6b1a7728a2c076fabc0607a408a
John Ballon <ballonj1@tcnj.edu>
2017-05-16 10:48:08 -0700

    Start applicaton controller methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,34 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = user.find_by(id: params[:id])
+    if @user
+      render "api/users/show"
+    else
+      render json: @users.errors.full_messages, status: 422
+    end
+  end
+
+  def edit
+    @user = user.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+      render "api/users/show"
+    else
+      render json: @users.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,35 @@ class User < ApplicationRecord
   :session_token, :email, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  attr_reader :password
+
   has_many :joined_events
   has_many :hosts
 
   after_initialize :ensure_session_token
 
-  def find_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
-    
+    return user if user && user.valid_password?(password)
+    nil
   end
 
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save
+    self.session_token
+  end
 end
