ad8663712b78ec1d70dcc3e1782a47166d71606d
John Ballon <ballonj1@tcnj.edu>
2017-05-25 21:16:53 -0700

    Update dashboard join detail

--- frontend/components/dashboard_show/dashboard_joins_detail_container.js
+++ frontend/components/dashboard_show/dashboard_joins_detail_container.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux';
 import DashboardJoinsDetail from './dashboard_joins_detail';
+import { leaveEvent } from '../../actions/join_actions';
 
 const mapStateToProps = ({ join }) => ({
   join
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  leaveEvent: (user_id, event_id) => dispatch(leaveEvent(user_id, event_id))
+});
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(DashboardJoinsDetail);
